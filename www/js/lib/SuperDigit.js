c1632a8519fafa16c2ca0fb3019969f7e00eb2e5
Alvaro Silveira <alvaro.silveira@lighthouseit.com.br>
2018-02-20 00:08:52 -0300

    refactor

--- www/js/lib/SuperDigit.js
+++ www/js/lib/SuperDigit.js
@@ -1,29 +1,22 @@
 "use strict";
 
-class SuperDigit {
-  constructor(n, k) {
-    this.n = n;
-    this.k = Number(k);
+function getSum(n, k) {
+  var sum = 0;
+  for(var i = 0; i < n.length; i++) {
+    sum += Number(n[i]);
   }
+  return k? String(sum * k): String(sum);
+}
 
-  getOutput() {
-    if(
-      (Number(this.n) >= 1) &&
-      (this.k >= 1 && this.k <= Math.pow(10, 5))
-    ) {
-      let output = this._getSumNumbers(this.n, true);
-      while(output.length > 1) {
-        output = this._getSumNumbers(output);
-      }
-      return output;
-    }else return 0;
-  }
-
-  _getSumNumbers(n, isMultiple=false) {
-    let sum = 0;
-    for(let i = 0; i < n.length; i++) {
-      sum += Number(n[i]);
+function superDigit(n, k) {
+  if(
+    (Number(n) >= 1) &&
+    (k >= 1 && k <= Math.pow(10, 5))
+  ) {
+    var output = getSum(n, k);
+    while(output.length > 1) {
+      output = getSum(output);
     }
-    return isMultiple? String(sum * this.k): String(sum);
-  }
+    return output;
+  }else return 0;
 }
--- www/js/main.js
+++ www/js/main.js
@@ -1,81 +1,73 @@
 "use strict";
 
-class Main {
-  constructor() {
-    this.superDigit = null;
-    this.form = document.querySelector("form");
-    this.input = this.form.querySelector("input");
-    this.input.focus();
-    this.btnClear = this.form.querySelector(".btn-clear");
-    this.btnUpload = this.form.querySelector(".btn-upload");
-    this.content = document.querySelector(".content");
-    this.contentIn = this.content.querySelector(".in");
-    this.contentOut = this.content.querySelector(".out");
-  }
+var form = document.querySelector("form");
+var input = form.querySelector("input");
+input.focus();
+var btnClear = form.querySelector(".btn-clear");
+var btnUpload = form.querySelector(".btn-upload");
+var content = document.querySelector(".content");
+var contentIn = content.querySelector(".in");
+var contentOut = content.querySelector(".out");
 
-  submit(e) {
-    e.preventDefault();
-    this._showContent();
-    const value = this.input.value;
-    this.superDigit = new SuperDigit(...value.split(' '));
-    this.btnClear.removeAttribute("disabled");
-    this.btnUpload.setAttribute("disabled", '');
-    const output = this.superDigit.getOutput();
-    if(output) {
-      this._setIn(value);
-      this._setOut(output);
-    }
-    this.input.value = '';
-  }
-
-  clear() {
-    this.form.removeAttribute("style");
-    this.content.removeAttribute("style");
-    this.contentIn.innerHTML = "<h4>Input</h4>";
-    this.contentOut.innerHTML = "<h4>Output</h4>";
-    this.input.removeAttribute("disabled");
-    this.btnClear.setAttribute("disabled", '');
-    this.btnUpload.removeAttribute("disabled");
-    this.input.focus();
-    this.step = 0;
-  }
-
-  upload() {
-    let input = document.createElement("input");
-    input.setAttribute("type", "file");
-    input.addEventListener("change", e => {
-      let reader = new FileReader();
-      reader.onload = function() {
-        this._showContent();
-        let result = reader.result.split('\n');
-        for(let i = 0; i < result.length; i++) {
-          if(result[i]) {
-            this.input.value = result[i];
-            this.submit(e);
-          }
-        }
-      }.bind(this)
-      reader.readAsText(input.files[0]);
-    })
-    input.click();
-    return false;
-  }
-
-  _showContent() {
-    this.form.style.top = "120px";
-    this.content.style.opacity = '1';
-  }
-
-  _setIn(text) {
-    let p = document.createElement('p');
-    p.textContent = text;
-    this.contentIn.appendChild(p);
-  }
-
-  _setOut(text) {
-    let p = document.createElement('p');
-    p.textContent = text;
-    this.contentOut.appendChild(p);
-  }
+function showContent() {
+  form.style.top = "120px";
+  content.style.opacity = '1';
 }
-const main = new Main();
+
+function setValue(text, name) {
+  let p = document.createElement('p');
+  p.textContent = text;
+  if(name === "in")
+    contentIn.appendChild(p);
+  else if(name === "out")
+    contentOut.appendChild(p);
+}
+
+function submit(e) {
+  e.preventDefault();
+  showContent();
+  var value = input.value;
+  btnClear.removeAttribute("disabled");
+  btnUpload.setAttribute("disabled", '');
+  var output = superDigit(...value.split(' '));
+  if(output) {
+    setValue(value, "in");
+    setValue(output, "out");
+  }
+  input.value = '';
+}
+
+function clear() {
+  form.removeAttribute("style");
+  content.removeAttribute("style");
+  contentIn.innerHTML = "<h4>Input</h4>";
+  contentOut.innerHTML = "<h4>Output</h4>";
+  input.removeAttribute("disabled");
+  btnClear.setAttribute("disabled", '');
+  btnUpload.removeAttribute("disabled");
+  input.focus();
+}
+
+function upload(e) {
+  var inputFile = document.createElement("input");
+  inputFile.setAttribute("type", "file");
+  inputFile.addEventListener("change", e => {
+    var reader = new FileReader();
+    reader.onload = function() {
+      showContent();
+      var result = reader.result.split('\n');
+      for(var i = 0; i < result.length; i++) {
+        if(result[i]) {
+          input.value = result[i];
+          submit(e);
+        }
+      }
+    }
+    reader.readAsText(inputFile.files[0]);
+  })
+  inputFile.click();
+}
+
+form.addEventListener("submit", submit);
+btnClear.addEventListener("click", clear);
+btnUpload.addEventListener("click", upload);
